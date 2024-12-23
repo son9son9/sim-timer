6dd7744a948e79ba2d731422f83f3fb6d6dd3d31
songsong <dragonh0919@gmail.com>
2024-12-24 02:30:26 +0900

    Fix: 이미지 상대경로 수정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/sim-timer",
+  assetPrefix: "sim-timer",
 };
 
 export default nextConfig;
