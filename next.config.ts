51388bf0fb9c96f913550081cf5cca2c5c71cfd5
songsong <dragonh0919@gmail.com>
2025-01-12 00:49:42 +0900

    build: next.config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: isProduction ? productionURL : "",
   assetPrefix: isProduction ? productionURL : "",
+  output: "export", // Static HTML Export를 위해 필요
+  images: {
+    unoptimized: true, // GitHub Pages 배포를 위해 필요
+    loader: "custom", // 커스텀 로더 설정
+  },
+  trailingSlash: true, // URL 끝에 슬래시 추가
 };
 
 export default nextConfig;
